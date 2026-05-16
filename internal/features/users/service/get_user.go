e66c24dc36a095498c868a7e4d61ba891ecafdea
Sanichka <perdak487@gmail.com>
2026-05-16 17:55:54 +0300

    implemented get_user feature

---
+++ internal/features/users/service/get_user.go
@@ -0,0 +1,23 @@
+package users_service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/odelshchwank/BigProjectLesson/internal/core/domain"
+)
+
+func (s *UsersService) GetUser(
+	ctx context.Context,
+	id int,
+) (domain.User, error) {
+	user, err := s.usersRepostitory.GetUser(ctx, id)
+	if err != nil {
+		return domain.User{}, fmt.Errorf(
+			"get user from repository: %w",
+			err,
+		)
+	}
+
+	return user, nil
+}
