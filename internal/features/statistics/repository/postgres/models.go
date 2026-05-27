ee89cb63c55aa85f4febe573d6ef5cae8da3a6e3
Sanichka <perdak487@gmail.com>
2026-05-28 00:36:14 +0300

    implemented Statistics feature

---
+++ internal/features/statistics/repository/postgres/models.go
@@ -0,0 +1,42 @@
+package statistics_postgres_repository
+
+import (
+	"time"
+
+	"github.com/odelshchwank/BigProjectLesson/internal/core/domain"
+)
+
+type TaskModel struct {
+	ID           int
+	Version      int
+	Title        string
+	Description  *string
+	Completed    bool
+	CreatedAt    time.Time
+	CompletedAt  *time.Time
+	AuthorUserID int
+}
+
+func taskDomainsFromModels(taskModels []TaskModel) []domain.Task {
+	domains := make([]domain.Task, len(taskModels))
+
+	for i, model := range taskModels {
+		domains[i] = taskDomainFromModel(model)
+	}
+
+	return domains
+}
+
+func taskDomainFromModel(task TaskModel) domain.Task {
+	return domain.NewTask(
+		task.ID,
+		task.Version,
+		task.Title,
+		task.Description,
+		task.Completed,
+		task.CreatedAt,
+		task.CompletedAt,
+		task.AuthorUserID,
+	)
+
+}
