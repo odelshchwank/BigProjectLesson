3a1a7141084f78640abfb6abb6b3e0bb890ebf00
odelshchwank <alexandr.hlinsky@gmail.com>
2026-05-29 17:35:00 +0300

    finished frontend

---
+++ internal/features/web/repository/file_system/get_file.go
@@ -0,0 +1,29 @@
+package web_fs_repository
+
+import (
+	"fmt"
+	"os"
+
+	core_errors "github.com/odelshchwank/BigProjectLesson/internal/core/errors"
+)
+
+func (r *WebRepository) GetFile(filepath string) ([]byte, error) {
+	file, err := os.ReadFile(filepath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf(
+				"file: %s: %w",
+				filepath,
+				core_errors.ErrNotFound,
+			)
+		}
+
+		return nil, fmt.Errorf(
+			"get file: %s: %w",
+			filepath,
+			err,
+		)
+	}
+
+	return file, nil
+}
